975fbef705bce386be80d9579c2f21e7f43e0eb2
Sascha Narr <saschanarr@googlemail.com>
2015-11-04 17:38:47 +0100

    Upgrade south migrations.

--- cosinnus_stream/migrations/0001_initial.py
+++ cosinnus_stream/migrations/0001_initial.py
@@ -1,165 +1,40 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+import cosinnus_stream.mixins
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Stream'
-        db.create_table(u'cosinnus_stream_stream', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('media_tag', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cosinnus.TagObject'], unique=True, null=True, on_delete=models.PROTECT, blank=True)),
-            ('group', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name=u'cosinnus_stream_stream_set', null=True, on_delete=models.PROTECT, to=orm['cosinnus.CosinnusGroup'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=55, blank=True)),
-            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'cosinnus_stream_stream_set', on_delete=models.PROTECT, to=orm['auth.User'])),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal(u'cosinnus_stream', ['Stream'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('cosinnus', '0001_initial'),
+    ]
 
-        # Adding unique constraint on 'Stream', fields ['creator', 'slug']
-        db.create_unique(u'cosinnus_stream_stream', ['creator_id', 'slug'])
-
-        # Adding M2M table for field attached_objects on 'Stream'
-        m2m_table_name = db.shorten_name(u'cosinnus_stream_stream_attached_objects')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('stream', models.ForeignKey(orm[u'cosinnus_stream.stream'], null=False)),
-            ('attachedobject', models.ForeignKey(orm[u'cosinnus.attachedobject'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['stream_id', 'attachedobject_id'])
-
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'Stream', fields ['creator', 'slug']
-        db.delete_unique(u'cosinnus_stream_stream', ['creator_id', 'slug'])
-
-        # Deleting model 'Stream'
-        db.delete_table(u'cosinnus_stream_stream')
-
-        # Removing M2M table for field attached_objects on 'Stream'
-        db.delete_table(db.shorten_name(u'cosinnus_stream_stream_attached_objects'))
-
-
-    models = {
-        u'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'cosinnus.attachedobject': {
-            'Meta': {'ordering': "(u'content_type',)", 'unique_together': "((u'content_type', u'object_id'),)", 'object_name': 'AttachedObject'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
-        },
-        u'cosinnus.cosinnusgroup': {
-            'Meta': {'ordering': "(u'name',)", 'object_name': 'CosinnusGroup'},
-            'avatar': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'description': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'media_tag': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['cosinnus.TagObject']", 'unique': 'True', 'null': 'True', 'on_delete': 'models.PROTECT', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'organisation': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "u'groups'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': u"orm['cosinnus.CosinnusOrganisation']"}),
-            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'blank': 'True'}),
-            'users': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'cosinnus_groups'", 'blank': 'True', 'through': u"orm['cosinnus.CosinnusGroupMembership']", 'to': u"orm['auth.User']"})
-        },
-        u'cosinnus.cosinnusgroupmembership': {
-            'Meta': {'unique_together': "((u'user', u'group'),)", 'object_name': 'CosinnusGroupMembership'},
-            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'memberships'", 'to': u"orm['cosinnus.CosinnusGroup']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'status': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0', 'db_index': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'cosinnus_memberships'", 'to': u"orm['auth.User']"})
-        },
-        u'cosinnus.cosinnusorganisation': {
-            'Meta': {'ordering': "(u'name',)", 'object_name': 'CosinnusOrganisation'},
-            'avatar': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'description': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'media_tag': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['cosinnus.TagObject']", 'unique': 'True', 'null': 'True', 'on_delete': 'models.PROTECT', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'blank': 'True'}),
-            'users': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'cosinnus_organisations'", 'blank': 'True', 'through': u"orm['cosinnus.CosinnusOrganisationMembership']", 'to': u"orm['auth.User']"})
-        },
-        u'cosinnus.cosinnusorganisationmembership': {
-            'Meta': {'unique_together': "((u'user', u'organisation'),)", 'object_name': 'CosinnusOrganisationMembership'},
-            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'organisation': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'memberships'", 'to': u"orm['cosinnus.CosinnusOrganisation']"}),
-            'status': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0', 'db_index': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'cosinnus_organisation_memberships'", 'to': u"orm['auth.User']"})
-        },
-        u'cosinnus.tagobject': {
-            'Meta': {'object_name': 'TagObject'},
-            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'+'", 'null': 'True', 'to': u"orm['cosinnus.CosinnusGroup']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'location_place': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'blank': 'True'}),
-            'people_name': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'blank': 'True'}),
-            'persons': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "u'+'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['auth.User']"}),
-            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'visibility': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1', 'blank': 'True'})
-        },
-        u'cosinnus_stream.stream': {
-            'Meta': {'unique_together': "((u'creator', u'slug'),)", 'object_name': 'Stream'},
-            'attached_objects': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['cosinnus.AttachedObject']", 'null': 'True', 'blank': 'True'}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
-            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'cosinnus_stream_stream_set'", 'on_delete': 'models.PROTECT', 'to': u"orm['auth.User']"}),
-            'group': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "u'cosinnus_stream_stream_set'", 'null': 'True', 'on_delete': 'models.PROTECT', 'to': u"orm['cosinnus.CosinnusGroup']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'media_tag': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['cosinnus.TagObject']", 'unique': 'True', 'null': 'True', 'on_delete': 'models.PROTECT', 'blank': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '55', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        u'taggit.tag': {
-            'Meta': {'object_name': 'Tag'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'})
-        },
-        u'taggit.taggeditem': {
-            'Meta': {'object_name': 'TaggedItem'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_taggeditem_tagged_items'", 'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
-            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_taggeditem_items'", 'to': u"orm['taggit.Tag']"})
-        }
-    }
-
-    complete_apps = ['cosinnus_stream']
+    operations = [
+        migrations.CreateModel(
+            name='Stream',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255, verbose_name='Title')),
+                ('slug', models.SlugField(max_length=55, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='Created')),
+                ('models', models.CharField(max_length=255, null=True, verbose_name='Models', blank=True)),
+                ('is_my_stream', models.BooleanField(default=False)),
+                ('portals', models.CommaSeparatedIntegerField(default='', max_length=255, verbose_name='Portal IDs', blank=True)),
+                ('last_seen', models.DateTimeField(default=None, null=True, verbose_name='Last seen', blank=True)),
+                ('attached_objects', models.ManyToManyField(to='cosinnus.AttachedObject', null=True, blank=True)),
+                ('creator', models.ForeignKey(related_name='cosinnus_stream_stream_set', verbose_name='Creator', to=settings.AUTH_USER_MODEL)),
+                ('group', models.ForeignKey(related_name='cosinnus_stream_stream_set', verbose_name='Group', blank=True, to='cosinnus.CosinnusGroup', null=True)),
+            ],
+            options={
+                'ordering': ['created'],
+                'abstract': False,
+                'verbose_name': 'Stream',
+                'verbose_name_plural': 'Streams',
+            },
+            bases=(cosinnus_stream.mixins.StreamManagerMixin, models.Model),
+        ),
+    ]
