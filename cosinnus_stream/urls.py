06f2e57e8fdaed127fa474a12d72c0a4abb7fe5b
Sascha Narr <saschanarr@googlemail.com>
2015-11-09 17:26:16 +0100

    All stream views disabled for integrated portals

--- cosinnus_stream/urls.py
+++ cosinnus_stream/urls.py
@@ -2,18 +2,22 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
+from cosinnus.templatetags.cosinnus_tags import is_integrated_portal
 
 
-cosinnus_root_patterns = patterns('',
-                                   
-    url(r'^activities/create/$', 'cosinnus_stream.views.stream_create', name='create_stream'),
-    url(r'^activities/all/', 'cosinnus_stream.views.stream_detail', name='stream_public', kwargs={'is_all_portals': True}),
-    url(r'^activities/(?P<slug>[^/]+)/$', 'cosinnus_stream.views.stream_detail', name='stream'),
-    url(r'^activities/(?P<slug>[^/]+)/edit/$', 'cosinnus_stream.views.stream_update', name='edit_stream'),
-    url(r'^activities/(?P<slug>[^/]+)/delete/$', 'cosinnus_stream.views.stream_delete', name='delete_stream'),
-    url(r'^activities/$', 'cosinnus_stream.views.stream_detail', name='my_stream'),
-)
-
+# user management not allowed in integrated mode
+if not is_integrated_portal():
+    cosinnus_root_patterns = patterns('',
+                                       
+        url(r'^activities/create/$', 'cosinnus_stream.views.stream_create', name='create_stream'),
+        url(r'^activities/all/', 'cosinnus_stream.views.stream_detail', name='stream_public', kwargs={'is_all_portals': True}),
+        url(r'^activities/(?P<slug>[^/]+)/$', 'cosinnus_stream.views.stream_detail', name='stream'),
+        url(r'^activities/(?P<slug>[^/]+)/edit/$', 'cosinnus_stream.views.stream_update', name='edit_stream'),
+        url(r'^activities/(?P<slug>[^/]+)/delete/$', 'cosinnus_stream.views.stream_delete', name='delete_stream'),
+        url(r'^activities/$', 'cosinnus_stream.views.stream_detail', name='my_stream'),
+    )
+else:
+    cosinnus_root_patterns = []
 
 cosinnus_group_patterns = patterns('cosinnus_stream.views',
 )
