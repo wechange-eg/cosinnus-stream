938b0590d4db6e66785565bc2c9175aa1a9b4bef
Sascha Narr <saschanarr@googlemail.com>
2018-09-26 18:59:00 +0200

    django1.9 prep - URLs and imports

--- cosinnus_stream/urls.py
+++ cosinnus_stream/urls.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from cosinnus.templatetags.cosinnus_tags import is_integrated_portal
-
+from cosinnus_stream import views
 
 # user management not allowed in integrated mode
 if not is_integrated_portal():
-    cosinnus_root_patterns = patterns('',
-                                       
-        url(r'^activities/create/$', 'cosinnus_stream.views.stream_create', name='create_stream'),
-        url(r'^activities/all/', 'cosinnus_stream.views.stream_detail', name='stream_public', kwargs={'is_all_portals': True}),
-        url(r'^activities/(?P<slug>[^/]+)/$', 'cosinnus_stream.views.stream_detail', name='stream'),
-        url(r'^activities/(?P<slug>[^/]+)/edit/$', 'cosinnus_stream.views.stream_update', name='edit_stream'),
-        url(r'^activities/(?P<slug>[^/]+)/delete/$', 'cosinnus_stream.views.stream_delete', name='delete_stream'),
-        url(r'^activities/$', 'cosinnus_stream.views.stream_detail', name='my_stream'),
-    )
+    cosinnus_root_patterns = [
+        url(r'^activities/create/$', views.stream_create, name='create_stream'),
+        url(r'^activities/all/', views.stream_detail, name='stream_public', kwargs={'is_all_portals': True}),
+        url(r'^activities/(?P<slug>[^/]+)/$', views.stream_detail, name='stream'),
+        url(r'^activities/(?P<slug>[^/]+)/edit/$', views.stream_update, name='edit_stream'),
+        url(r'^activities/(?P<slug>[^/]+)/delete/$', views.stream_delete, name='delete_stream'),
+        url(r'^activities/$', views.stream_detail, name='my_stream'),
+    ]
 else:
     cosinnus_root_patterns = []
 
-cosinnus_group_patterns = patterns('cosinnus_stream.views',
-)
+cosinnus_group_patterns = []
+
 
 urlpatterns = cosinnus_group_patterns + cosinnus_root_patterns
