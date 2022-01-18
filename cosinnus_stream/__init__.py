0c248121fae12e7a42b6291013addb3bcef9590e
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-01-18 17:55:47 +0100

    remove default_app_config is as it’s deprecated.

--- cosinnus_stream/__init__.py
+++ cosinnus_stream/__init__.py
@@ -5,8 +5,6 @@ from __future__ import unicode_literals
 from builtins import str
 VERSION = (0, 5, 0, 'alpha', 0)
 
-default_app_config = 'cosinnus_stream.apps.CosinnusStreamAppConfig'
-
 
 def get_version(full=True):
     """Derives a PEP386-compliant version number from VERSION.
